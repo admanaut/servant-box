948208614b21d5e2876310d6d635c5e741451ef2
Adrian Marina <adrianmarina.adrian@gmail.com>
2018-12-06 08:28:01 +0000

    Use Servant Client to consume the Reminders API

--- lib/client/src/Main.hs
+++ lib/client/src/Main.hs
@@ -1,9 +1,11 @@
 module Main where
 
-import Servant.Client
-import Server
-import Reminders
-import Servant
+import           Servant.Client
+import           Server
+import           Reminders
+import           Servant
+import qualified Data.Time           as T
+import qualified Network.HTTP.Client as HTTP
 
 getReminders :: ClientM [WithId Reminder]
 postReminder :: Reminder -> ClientM [WithId Reminder]
@@ -13,4 +15,22 @@ getReminders :<|> postReminder :<|> deleteReminder :<|> putReminder
   = client api
 
 main :: IO ()
-main = pure ()
+main = do
+  baseUrl <- parseBaseUrl "http://localhost:8080"
+  manager <- HTTP.newManager HTTP.defaultManagerSettings
+  let env = mkClientEnv manager baseUrl
+
+  print =<< runClientM getReminders env
+  print =<< runClientM (postReminder newReminder) env
+  print =<< runClientM (deleteReminder 1001) env
+  print =<< runClientM (putReminder 1001 newReminder) env
+
+  where
+    newReminder =
+      Reminder
+       { title    = "Wake up"
+       , onADay   = T.UTCTime (T.fromGregorian 2019 11 14) (T.secondsToDiffTime 540)
+       , repeat   = NoRepeat
+       , priority = High
+       , note     = Nothing
+       }
